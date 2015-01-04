9606684c8b11700a76c9541fb48b735e6034db3e
Michael Tyson <michael@atastypixel.com>
2015-01-04 13:33:52 +1100

    Added performance profiling

--- AudioTest/AppDelegate.m
+++ AudioTest/AppDelegate.m
@@ -9,6 +9,11 @@
 #import "AppDelegate.h"
 #import <AudioToolbox/AudioToolbox.h>
 #import <AVFoundation/AVFoundation.h>
+#import <mach/mach_time.h>
+#import <libkern/OSAtomic.h>
+
+static double __hostTicksToSeconds = 0.0;
+static double __secondsToHostTicks = 0.0;
 
 #define checkResult(result,operation) (_checkResult((result),(operation),strrchr(__FILE__, '/')+1,__LINE__))
 static inline BOOL _checkResult(OSStatus result, const char *operation, const char* file, int line) {
@@ -19,17 +24,32 @@ static inline BOOL _checkResult(OSStatus result, const char *operation, const ch
     return YES;
 }
 
+static const int kMaxTimingEntries = 256;
+
+struct timing_entry_t { uint64_t timestamp; uint64_t start; uint64_t end; };
+
 @interface AppDelegate () {
     AudioUnit _audioUnit;
+    struct timing_entry_t _timingEntries[kMaxTimingEntries];
+    volatile int32_t _timingEntriesHead;
+    volatile int32_t _timingEntriesTail;
 }
 @property (nonatomic, strong) id observerToken;
 @end
 
 @implementation AppDelegate
 
++(void)initialize {
+    mach_timebase_info_data_t tinfo;
+    mach_timebase_info(&tinfo);
+    __hostTicksToSeconds = ((double)tinfo.numer / tinfo.denom) * 1.0e-9;
+    __secondsToHostTicks = 1.0 / __hostTicksToSeconds;
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [self setupAudioSystem];
     [self startAudioSystem];
+    [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(pollTimingEntries) userInfo:nil repeats:YES];
     return YES;
 }
 
@@ -145,7 +165,11 @@ static inline BOOL _checkResult(OSStatus result, const char *operation, const ch
 }
 
 static OSStatus audioUnitRenderCallback(void *inRefCon, AudioUnitRenderActionFlags *ioActionFlags, const AudioTimeStamp *inTimeStamp, UInt32 inBusNumber, UInt32 inNumberFrames, AudioBufferList *ioData) {
+    __unsafe_unretained AppDelegate * THIS = (__bridge AppDelegate*)inRefCon;
     
+    uint64_t start = mach_absolute_time();
+    
+    // Quick sin-esque oscillator
     const float oscillatorFrequency = 400.0;
     static float oscillatorPosition = 0.0;
     float oscillatorRate = oscillatorFrequency / 44100.0;
@@ -158,7 +182,29 @@ static OSStatus audioUnitRenderCallback(void *inRefCon, AudioUnitRenderActionFla
         ((float*)ioData->mBuffers[1].mData)[i] = x;
     }
     
+    uint64_t end = mach_absolute_time();
+    
+    int32_t head = THIS->_timingEntriesHead;
+    int32_t tail = THIS->_timingEntriesTail;
+    if ( (head+1)%kMaxTimingEntries == tail ) {
+        NSLog(@"Timing buffer full");
+    } else {
+        THIS->_timingEntries[head] = (struct timing_entry_t){ .timestamp = inTimeStamp->mHostTime, .start = start, .end = end };
+        OSAtomicCompareAndSwap32Barrier(head, (head+1) % kMaxTimingEntries, &THIS->_timingEntriesHead);
+    }
+    
     return noErr;
+}
+
+- (void)pollTimingEntries {
+    for ( int i=_timingEntriesTail; i != _timingEntriesHead; i = (i+1)%kMaxTimingEntries ) {
+        NSLog(@"%lf: render start %lf, end %lf, duration %lf ms",
+              _timingEntries[i].timestamp * __hostTicksToSeconds,
+              _timingEntries[i].start * __hostTicksToSeconds,
+              _timingEntries[i].end * __hostTicksToSeconds,
+              (_timingEntries[i].end - _timingEntries[i].start) * __hostTicksToSeconds * 1000.0);
+        OSAtomicCompareAndSwap32Barrier(i, (i+1)%kMaxTimingEntries, &_timingEntriesTail);
+    }
 }
 
 @end
